5cbcb553cb67b06c6c719bdd2444726aae18ee6e
András Veres-Szentkirályi <vsza@vsza.hu>
2015-01-01 16:20:38 +0100

    made onCreate parameter final

--- android/src/org/hsbp/spares/android/Connect.java
+++ android/src/org/hsbp/spares/android/Connect.java
@@ -7,7 +7,7 @@ public class Connect extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
